fcb8005d0ab7190c3c7652d0b16d7e5a41ef770a
marcelgualberto <marcel.gualberto@hotmail.com>
2021-11-18 20:31:04 -0300

    correção fotter

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,12 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Dutrafae" 
-                target="_blank" rel="noreferrer">Arthur</a>
-                </p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" 
-                    target="_blank" rel="noreferrer">@devsuperior.ig</a></small>
+                    Criado por  <a href="https://instagram.com/marcelgualberto" 
+                    target="_blank" rel="noreferrer">Projeto</a></small>
                 </p>
             </div>
         </footer>
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,9 +6,7 @@ const Navbar = () => {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <Link to="/">
-                        <img src={ImgDsDark} alt="DevSuperior" width="120" />
-                    </Link>
+                   
                 </nav>
             </div>
         </div>
